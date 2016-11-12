1bfe9ecf68d3726cdc882bd51b5d6e2adcb1d5a2
Avik Hasija <avik.hasija@uwaterloo.ca>
2016-11-11 20:33:55 -0500

    Errors and log in screen

---
+++ Android/JobMine/app/src/main/java/com/avikhasija/jobmine/LogIn.java
@@ -0,0 +1,33 @@
+package com.avikhasija.jobmine;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LogIn extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+
+        //fetch UI elements
+        Button signInButton = (Button) findViewById(R.id.sign_in_button);
+        final EditText username = (EditText) findViewById(R.id.waterloo_id);
+        EditText password = (EditText) findViewById(R.id.sign_in_password);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = username.getText().toString();
+                Intent i = new Intent(LogIn.this, MainActivity.class);
+                startActivity(i);
+                //TODO: determine student or employer from json
+                //load main activity, different layout for each
+            }
+        });
+    }
+}
